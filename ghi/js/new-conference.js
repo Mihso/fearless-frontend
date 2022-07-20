a441e68ae28dedb6ee1dd8f2ed173f4f41610492
Matthew Oshimo <moshimo90696@gmail.com>
2022-07-20 15:39:17 -0700

    conference creation

---
+++ ghi/js/new-conference.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = "http://localhost:8000/api/locations/";
+    try{
+        const response = await fetch(url);
+        if(!response.ok)
+        {
+            pass
+        }
+        else
+        {
+            
+            const data = await response.json();
+            console.log(data)
+            
+            const selection = document.getElementById('Location')
+            for( let c of data.locations)
+            {
+                const i = document.createElement('option')
+                i.value = c.id
+                i.innerHTML = c.name
+                console.log(i)
+                selection.appendChild(i)
+            }
+
+        }
+    }
+    catch(e){
+        console.log("error", e)
+    }
+
+        const formTag = document.getElementById('create-conference-form');
+        formTag.addEventListener('submit', async event => {
+            event.preventDefault();
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+            const locationUrl = 'http://localhost:8000/api/conferences/';
+            const fetchConfig = {
+            method: "post",
+            body: json,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            };
+            const response = await fetch(locationUrl, fetchConfig);
+            if (response.ok) {
+            formTag.reset();
+            const newLocation = await response.json();
+            console.log(newLocation);
+            }
+        });
+});
